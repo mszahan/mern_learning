f30e9becfc24a9dba8b46508909e030ee4be1a5a
mszahan <mszahan1@gmail.com>
2024-06-09 18:46:53 +0600

    added connection to axios and added player list component

--- soccer/frontend/src/components/App.jsx
+++ soccer/frontend/src/components/App.jsx
@@ -1,17 +1,48 @@
 import React from "react";
+import axios from "axios";
 import "./App.css";
 import PlayerForm from "./player/PlayerForm";
 import PlayerList from "./player/PlayerList";
 import PlayerDetail from "./player/PlayerDetail";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      players: [],
+      currentPlayer: {},
+    };
+  }
+
+  componentDidMount() {
+    const url = "http://localhost:4000/players";
+
+    axios
+      .get(url)
+      .then((Response) => {
+        this.setState({
+          players: Response.data,
+        });
+      })
+      .catch((error) => console.log(error));
+  }
+
+  updateCurrentPlayer(player) {
+    this.setState({
+      currentPlayer: player,
+    });
+  }
+
   render() {
     return (
       <div className="container">
         <div className="menu">Menu</div>
 
         <div className="player_list">
-          <PlayerList />
+          <PlayerList
+            players={this.state.players}
+            updateCurrentPlayer={this.updateCurrentPlayer}
+          />
         </div>
         <div className="player_detail">
           <PlayerDetail />
--- soccer/frontend/src/components/player/PlayerList.jsx
+++ soccer/frontend/src/components/player/PlayerList.jsx
@@ -1,7 +1,18 @@
-const PlayerList = () => {
+const PlayerList = (props) => {
   return (
     <div>
       <h1>Player List</h1>
+      <div className="plyers_name">
+        <ul>
+          {props.players.map((player) => (
+            <li key={player.id}>
+              <a href="#" onClick={props.updateCurrentPlayer.bind(player)}>
+                {player.firstName} {player.lastName}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
