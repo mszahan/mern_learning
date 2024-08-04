90a66bff4d6a1d79a145a3eda1db381a6f5aede9
mszahan <mszahan1@gmail.com>
2024-08-04 17:51:26 +0600

    explored params and query params

--- mosh/express-demo/index.js
+++ mosh/express-demo/index.js
@@ -10,6 +10,14 @@ app.get("/api/courses", (req, res) => {
   res.send(["python", "nodejs", "golang"]);
 });
 
+// reading param both query and normal
+
+app.get("/api/course/:id/:year", (req, res) => {
+  res.send(req.params);
+  // to send query parameters
+  // res.send(req.query);
+});
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`listening on port ${port}...`));
