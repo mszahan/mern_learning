21aec01b6df917fe30ba2d389d7d3f2c3c06ad20
mszahan <mszahan1@gmail.com>
2024-08-09 18:28:13 +0600

    added jwt and cookie for user registration

--- projects/booking/backend/src/index.ts
+++ projects/booking/backend/src/index.ts
@@ -1,7 +1,8 @@
 import express, { Request, Response } from "express";
+import mongoose, { mongo } from "mongoose";
 import cors from "cors";
 import "dotenv/config";
-import mongoose, { mongo } from "mongoose";
+import userRoutes from "./routes/users";
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
@@ -10,9 +11,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.get("/api/test", async (req: Request, res: Response) => {
-  res.json({ message: "Hello from express endpoint" });
-});
+app.use("/api/users", userRoutes);
+
+// app.get("/api/test", async (req: Request, res: Response) => {
+//   res.json({ message: "Hello from express endpoint" });
+// });
 
 app.listen(8000, () => {
   console.log("server has started at localhost:8000");
