c8b5ffa11a02c4dc1f5462b756b897ab69c21c40
mszahan <mszahan1@gmail.com>
2025-05-11 16:15:38 +0600

    just random commit

--- projects/booking/backend/src/index.ts
+++ projects/booking/backend/src/index.ts
@@ -10,7 +10,12 @@ mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
